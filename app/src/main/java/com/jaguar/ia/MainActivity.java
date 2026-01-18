82a63b2ec15fb8868859bdda02c8cad0e50c8069
Planalto Automação <phbuzo19@gmail.com>
2026-01-18 18:28:01 -0300

    Add MainActivity minimal working

--- app/src/main/java/com/jaguar/ia/MainActivity.java
+++ app/src/main/java/com/jaguar/ia/MainActivity.java
@@ -1,12 +1,18 @@
 package com.jaguar.ia;
 
+import android.app.Activity;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
     @Override
-    protected void onCreate(BundleInstanceState savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        TextView tv = new TextView(this);
+        tv.setText("JaguarIA funcionando 🚀");
+        tv.setTextSize(22);
+        setContentView(tv);
     }
 }
