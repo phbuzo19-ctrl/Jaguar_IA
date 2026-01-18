f23b55135ae68d6cca8f2b474058bdf4472f6624
Planalto Automação <phbuzo19@gmail.com>
2026-01-18 16:56:55 -0300

    Projeto Android Jaguar IA

---
+++ app/src/main/java/com/jaguar/ia/MainActivity.java
@@ -0,0 +1,12 @@
+package com.jaguar.ia;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(BundleInstanceState savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
